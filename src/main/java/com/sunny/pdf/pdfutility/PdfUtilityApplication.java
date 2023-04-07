c087b0ba62c366919dce6b63cf3a7f7029b05ecf
sunny <sunnydiatm.@gmail.com>
2023-04-07 12:29:39 +1000

    first commit

---
+++ src/main/java/com/sunny/pdf/pdfutility/PdfUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.pdf.pdfutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunny/pdf/pdfutility/PdfUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.pdf.pdfutility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
